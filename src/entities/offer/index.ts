54f88d0a4c6ea5b40b6674623d68f2491d6ce0dc
Кирилл Войтенко <frost5042@gmail.com>
2024-12-08 16:25:49 +0500

    Build fix

--- src/entities/offer/index.ts
+++ src/entities/offer/index.ts
@@ -1,4 +1,5 @@
-import type { MainOfferInfo, City, Location, OfferCityName, OfferType } from './model/types';
+import type { MainOfferInfo, City, OfferCityName, OfferType } from './model/types';
+import type { Location } from '@shared/types';
 import OfferCard from './ui/offer-card';
 
 export type { MainOfferInfo, City, Location, OfferCityName, OfferType };
