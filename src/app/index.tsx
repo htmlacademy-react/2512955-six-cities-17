50b670e370dc0bc3b95972639216720de6d8639b
Кирилл Войтенко <frost5042@gmail.com>
2024-12-25 00:41:48 +0500

    build fix

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider as ReduxStoreProvider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import store from './store';
-import { App } from './app';
+import { App } from './application';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
